8d5d3ffe414712e561a70897da1cba328bf73be4
pome1lo <paaworker@gmail.com>
2024-08-12 00:33:18 +0300

    add shadcn button

--- Bellini/client.react/src/layouts/BasicLayout.tsx
+++ Bellini/client.react/src/layouts/BasicLayout.tsx
@@ -1,5 +1,5 @@
 import {Outlet} from "react-router-dom";
-import {Header} from "../views/components/Header.tsx";
+import {Header} from "@/views/partials/Header.tsx";
 
 export const BasicLayout = () => {
     return (
