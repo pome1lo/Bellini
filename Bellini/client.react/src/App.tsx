dfff9c1b2a442f75b4ed3e86dfa31db443e0e352
pome1lo <paaworker@gmail.com>
2024-08-12 00:41:09 +0300

    add theme-provider

--- Bellini/client.react/src/App.tsx
+++ Bellini/client.react/src/App.tsx
@@ -6,11 +6,13 @@ import {MainPage} from "./views/pages/MainPage.tsx";
 function App() {
     return (
         <>
-            <Routes>
-                <Route path='/' element={<BasicLayout/>}>
-                    <Route index element={<MainPage/>}/>
-                </Route>
-            </Routes>
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <Routes>
+                    <Route path='/' element={<BasicLayout/>}>
+                        <Route index element={<MainPage/>}/>
+                    </Route>
+                </Routes>
+            </ThemeProvider>
         </>
     )
 }
