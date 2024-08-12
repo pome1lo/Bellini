17336f90522830830fd5d85cdd0edc892f529e0a
pome1lo <paaworker@gmail.com>
2024-08-13 01:24:17 +0300

    create login controller

---
+++ Bellini/AuthenticationService/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLogic.Services.DTOs;
+using BusinessLogic.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController
+    {
+        private readonly ILoginService _loginService;
+
+        public LoginController(ILoginService authService)
+        {
+            _loginService = authService;
+        }
+
+        [HttpPost]
+        public async Task<TokenDto?> Login([FromBody] LoginDto loginDto, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                return await _loginService.AuthenticateAsync(loginDto, cancellationToken);
+                
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return null;
+            }
+        }
+    }
+}
