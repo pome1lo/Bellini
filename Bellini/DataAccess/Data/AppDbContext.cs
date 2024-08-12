9d9c9579df5bd42f1d47434be93b5b3cfc77ba09
pome1lo <paaworker@gmail.com>
2024-08-12 23:23:39 +0300

    create App DB Context

---
+++ Bellini/DataAccess/Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Data.Configurations;
+using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        } 
+
+        public DbSet<User> Users { get; set; } = null!; 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { 
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOPALX\\SQLEXPRESS;Database=MODSEN_PRACTICE_SHOP;User Id=sa;Password=sa;TrustServerCertificate=true;");
+        }
+    }
+}
