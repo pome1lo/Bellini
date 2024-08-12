3f8e2c83938138d361fdac195ed8d5df02bad1c0
pome1lo <paaworker@gmail.com>
2024-08-13 00:29:44 +0300

    create ILoginService interface

---
+++ Bellini/BusinessLogic/Services/Interfaces/ILoginService.cs
@@ -0,0 +1,9 @@
+﻿using BusinessLogic.Services.DTOs;
+
+namespace BusinessLogic.Services.Interfaces
+{
+    public interface ILoginService
+    {
+        Task<string> AuthenticateAsync(LoginDto loginDto, CancellationToken cancellationToken = default);
+    }
+}
