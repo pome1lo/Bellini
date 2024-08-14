b83de4027501025e1c2074d434ae1d51c59b0c9e
pome1lo <paaworker@gmail.com>
2024-08-14 22:41:54 +0300

    create IPasswordService interface

---
+++ Bellini/BusinessLogicLayer/Services/Interfaces/IPasswordService.cs
@@ -0,0 +1,10 @@
+﻿namespace BusinessLogicLayer.Services.Interfaces
+{
+    public interface IPasswordService
+    {
+        Task ChangePasswordAsync(string userId, string currentPassword, string newPassword, CancellationToken cancellationToken = default);
+        Task ResetPasswordAsync(string userId, string newPassword, CancellationToken cancellationToken = default);
+        Task ForgotPasswordAsync(string email, CancellationToken cancellationToken = default);
+    }
+
+}
