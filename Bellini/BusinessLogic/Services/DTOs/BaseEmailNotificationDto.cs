8a1ea9c433df7d9b63284885cdf636c41ad306bf
pome1lo <paaworker@gmail.com>
2024-08-14 19:30:54 +0300

    create notification service

--- Bellini/BusinessLogic/Services/DTOs/BaseEmailNotificationDto.cs
+++ Bellini/BusinessLogic/Services/DTOs/BaseEmailNotificationDto.cs
@@ -5,5 +5,10 @@
         public string Email { get; set; }
         public string Subject { get; set; } = string.Empty;
         public string Body { get; set; } = string.Empty;
+
+        public string SmtpServer { get; set; }
+        public int SmtpPort { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; } 
     }
 }
---
+++ Bellini/BusinessLogic/Services/NotificationService.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLogicLayer.Services.DTOs;
+using BusinessLogicLayer.Services.Interfaces;
+using EmailSenderLibrary;
+
+namespace BusinessLogicLayer.Services
+{
+    public class NotificationService : INotificationService
+    {
+        public async Task SendEmailNotificationAsync(BaseEmailNotificationDto notificationDto, CancellationToken cancellationToken = default)
+        {
+            var emailSender = new EmailSender(
+                notificationDto.SmtpServer,
+                notificationDto.SmtpPort,
+                notificationDto.Username,
+                notificationDto.Password
+            );
+
+            await emailSender.SendEmailAsync(
+                "Bellini",
+                notificationDto.Username,
+                "", // Имя получателя
+                notificationDto.Email,
+                notificationDto.Subject,
+                notificationDto.Body
+            );
+        }
+    }
+}
