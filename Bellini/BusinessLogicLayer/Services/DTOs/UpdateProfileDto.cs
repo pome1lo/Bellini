be9328f66d6031cafd0170623ad9527bff69a167
pome1lo <paaworker@gmail.com>
2024-08-15 02:05:29 +0300

    create IProfileService interface / add UpdateProfileDto and ProfileDto DTO objects

---
+++ Bellini/BusinessLogicLayer/Services/DTOs/UpdateProfileDto.cs
@@ -0,0 +1,12 @@
+﻿namespace BusinessLogicLayer.Services.DTOs
+{
+    public class UpdateProfileDto
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? ProfileImageUrl { get; set; }
+    }
+}
