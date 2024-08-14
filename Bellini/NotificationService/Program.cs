cb110ac388e6deab64dbf5350a8a7b1c7773c404
pome1lo <paaworker@gmail.com>
2024-08-14 16:53:13 +0300

    configure the HTTP request pipeline

--- Bellini/NotificationService/Program.cs
+++ Bellini/NotificationService/Program.cs
@@ -1,34 +1,27 @@
+using AuthenticationService.MiddlewareExtensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//builder.Services.AddScoped<IRegisterService, RegisterService>();
 
+// Add controllers
+builder.Services.AddControllers();
+  
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure global exception handler
+app.UseGlobalExceptionHandler();
 
+// Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseRouting();
 
-app.MapGet("/weatherforecast", () =>
+app.UseEndpoints(endpoints =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    endpoints.MapControllers();
 });
 
-app.Run();
+app.MapGet("/", () => "Hello world!");
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.Run();
