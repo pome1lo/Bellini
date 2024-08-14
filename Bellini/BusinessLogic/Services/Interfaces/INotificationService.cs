353a6d728d48ca85878ce1c13f7dd75fc9668bda
pome1lo <paaworker@gmail.com>
2024-08-14 19:24:59 +0300

    create INotificationService interface

--- Bellini/BusinessLogic/Services/Interfaces/INotificationService.cs
+++ Bellini/BusinessLogic/Services/Interfaces/INotificationService.cs
@@ -1,7 +1,9 @@
-﻿namespace BusinessLogicLayer.Services.Interfaces
+﻿using BusinessLogicLayer.Services.DTOs;
+
+namespace BusinessLogicLayer.Services.Interfaces
 {
     public interface INotificationService
     {
-        
-    }
+        Task SendEmailNotificationAsync(BaseEmailNotificationDto notificationDto, CancellationToken cancellationToken = default);
+    } 
 }
