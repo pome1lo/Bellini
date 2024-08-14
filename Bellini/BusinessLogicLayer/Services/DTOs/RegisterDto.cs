f1218a16f3c1ed6c332fbe8cddb68753c51d6251
pome1lo <paaworker@gmail.com>
2024-08-15 01:03:32 +0300

    create ChangePasswordDtoValidator

--- Bellini/BusinessLogicLayer/Services/DTOs/RegisterDto.cs
+++ Bellini/BusinessLogicLayer/Services/DTOs/RegisterDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BusinessLogic.Services.DTOs
+﻿namespace BusinessLogic.Services.DTOs
 {
     public class RegisterDto
     {
