f357f41006136564beb157b2772a28f9af28ce43
pome1lo <paaworker@gmail.com>
2024-08-14 21:44:50 +0300

    fix email sender

--- Bellini/BusinessLogicLayer/Services/NotificationService.cs
+++ Bellini/BusinessLogicLayer/Services/NotificationService.cs
@@ -1,25 +1,32 @@
 ﻿using BusinessLogicLayer.Services.DTOs;
 using BusinessLogicLayer.Services.Interfaces;
+using BusinessLogicLayer.Utils;
 using EmailSenderLibrary;
 
 namespace BusinessLogicLayer.Services
 {
     public class NotificationService : INotificationService
     {
+        private readonly EmailSender _emailSender;
+        private readonly EmailSettingsDto _emailSettingsDto = ConfigurationHelper.GetEmailSettings();
+
+        public NotificationService()
+        {
+            _emailSettingsDto = ConfigurationHelper.GetEmailSettings();
+
+            _emailSender = new EmailSender(
+               _emailSettingsDto.SmtpServer,
+               _emailSettingsDto.SmtpPort,
+               _emailSettingsDto.Email,
+               _emailSettingsDto.Password
+            );
+        }
+
         public async Task SendEmailNotificationAsync(BaseEmailNotificationDto notificationDto, CancellationToken cancellationToken = default)
         {
-            var emailSender = new EmailSender(
-                notificationDto.SmtpServer,
-                notificationDto.SmtpPort,
-                notificationDto.Username,
-                notificationDto.Password
-            );
-
-            await emailSender.SendEmailAsync(
-                "Bellini",
-                notificationDto.Username,
-                "", // Имя получателя
-                notificationDto.Email,
+            await _emailSender.SendEmailAsync(
+                _emailSettingsDto.Email,
+                notificationDto.Email ?? _emailSettingsDto.Email,
                 notificationDto.Subject,
                 notificationDto.Body
             );
