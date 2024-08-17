619308a326bedbddbcbaa24383e781b6df73c1d9
pome1lo <paaworker@gmail.com>
2024-08-17 14:30:34 +0300

    add second ocelot file

--- Bellini/ApiGateway/Program.cs
+++ Bellini/ApiGateway/Program.cs
@@ -5,8 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+var isDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+var ocelotConfigFile = isDocker ? "ocelot.docker.json" : "ocelot.local.json";
+
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("Ocelot.json", optional: false, reloadOnChange: true)
+    .AddJsonFile(ocelotConfigFile, optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 
 builder.Services.AddOcelot(builder.Configuration);
