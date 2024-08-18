c1bcfe3689999d5c1318026053641bcb848be457
pome1lo <paaworker@gmail.com>
2024-08-18 23:33:22 +0300

    create register page

--- Bellini/client.react/src/views/pages/LoginPage.tsx
+++ Bellini/client.react/src/views/pages/LoginPage.tsx
@@ -13,9 +13,6 @@ import {
 import {Input} from "@/components/ui/input"
 import {Link, useNavigate} from "react-router-dom";
 import {serverFetch} from "@/utilds/fetch's/fetchServer.ts";
-import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "@/components/ui/carousel.tsx";
-import Autoplay from "embla-carousel-autoplay"
-import {Card, CardContent} from "@/components/ui/card.tsx";
 
 export const LoginPage = () => {
     const navigate = useNavigate();
@@ -52,61 +49,57 @@ export const LoginPage = () => {
     return (
         <>
             <div className="flex align-middle h-[78dvh] rounded-md">
-                <div className="w-1/2">
-
-                </div>
-                <div className="w-1/2">
-                    <div className="w-96 m-auto">
-                        <Form {...form}>
-                            <p className="font-bold text-2xl text-center">Login to your account</p>
-                            <p className="text-center ">Enter your login details in the our system.</p>
-                            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mt-2">
-                                <FormField
-                                    control={form.control}
-                                    name="email"
-                                    render={({field}) => (
-                                        <FormItem>
-                                            <FormLabel>Email</FormLabel>
-                                            <FormControl>
-                                                <Input type="email" {...field} required/>
-                                            </FormControl>
-                                            <FormMessage/>
-                                        </FormItem>
-                                    )}
-                                />
-                                <FormField
-                                    control={form.control}
-                                    name="password"
-                                    render={({field}) => (
-                                        <FormItem>
-                                            <FormLabel>Password</FormLabel>
-                                            <FormControl>
-                                                <Input type="password" {...field} required/>
-                                            </FormControl>
-                                            <FormMessage/>
-                                        </FormItem>
-                                    )}
-                                />
-                                <div className="text-end" style={{marginTop: '0.25rem'}}>
-                                    <Link to='/forgot-password'>Forgot password?</Link>
-                                </div>
-                                <Button type="submit" className="w-full">Login</Button>
-                                <div className="relative">
-                                    <div className="absolute inset-0 flex items-center"><span
-                                        className="w-full border-t"></span></div>
-                                    <div className="relative flex justify-center text-xs uppercase"><span
-                                        className="bg-background px-2 text-muted-foreground">Or continue with</span></div>
-                                </div>
-                                <Button onClick={() => navigate('/register')} variant="outline" className="w-full">Register</Button>
-                                <p className="px-8 text-center text-sm text-muted-foreground">
-                                    By clicking continue, you agree to our
-                                    <a className="underline hover:text-primary" href=""> Terms of Service </a>
-                                    and
-                                    <a className="underline hover:text-primary" href=""> Privacy Policy</a>.
-                                </p>
-                            </form>
-                        </Form>
-                    </div>
+                <div className="w-96 m-auto">
+                    <Form {...form}>
+                        <p className="font-bold text-2xl text-center">Login to your account</p>
+                        <p className="text-center ">Enter your login details in the our system.</p>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mt-2">
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input type="email" {...field} required/>
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <Input type="password" {...field} required/>
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                            <div className="text-end" style={{marginTop: '0.25rem'}}>
+                                <Link to='/forgot-password'>Forgot password?</Link>
+                            </div>
+                            <Button type="submit" className="w-full">Login</Button>
+                            <div className="relative">
+                                <div className="absolute inset-0 flex items-center"><span
+                                    className="w-full border-t"></span></div>
+                                <div className="relative flex justify-center text-xs uppercase"><span
+                                    className="bg-background px-2 text-muted-foreground">Or continue with</span></div>
+                            </div>
+                            <Button onClick={() => navigate('/register')} variant="outline"
+                                    className="w-full">Register</Button>
+                            <p className="px-8 text-center text-sm text-muted-foreground">
+                                By clicking continue, you agree to our
+                                <a className="underline hover:text-primary" href=""> Terms of Service </a>
+                                and
+                                <a className="underline hover:text-primary" href=""> Privacy Policy</a>.
+                            </p>
+                        </form>
+                    </Form>
                 </div>
             </div>
         </>
