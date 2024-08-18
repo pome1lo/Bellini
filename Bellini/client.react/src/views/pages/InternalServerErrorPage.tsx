e657bd6d2350c6958396a785883404376ce5bb3c
pome1lo <paaworker@gmail.com>
2024-08-19 01:41:06 +0300

    add mobile versions

--- Bellini/client.react/src/views/pages/InternalServerErrorPage.tsx
+++ Bellini/client.react/src/views/pages/InternalServerErrorPage.tsx
@@ -5,12 +5,14 @@ export const InternalServerErrorPage = () => {
     const navigate = useNavigate();
     return (
         <>
-            <div className="flex justify-center items-center h-screen flex-col">
+            <div className="flex justify-center items-center h-screen flex-col container">
                 <h1 className="font-black text-9xl text-roboto m-2 mt-3 animate-bounce">500</h1>
-                <h3 className="text-3xl font-medium">Sorry for the temporary inconvenience</h3>
-                <p className="m-2 mb-5 text-center">Try refreshing the page after a while or returning to the
-                    main <br></br> page. Sorry for the temporary inconvenience</p>
-                <Button className="w-96" onClick={() => navigate('/')}>Return to the main page</Button>
+                <h3 className="font-medium sm:text-3xl text-2xl text-center">Sorry for the temporary inconvenience</h3>
+                <div className="w-full md:w-1/2">
+                    <p className="m-2 mb-5 text-center">Try refreshing the page after a while or returning to the
+                        main page. Sorry for the temporary inconvenience</p>
+                </div>
+                <Button className="sm:w-96 w-72" onClick={() => navigate('/')}>Return to the main page</Button>
             </div>
         </>
     )
--- Bellini/client.react/src/views/pages/NotFoundPage.tsx
+++ Bellini/client.react/src/views/pages/NotFoundPage.tsx
@@ -5,11 +5,13 @@ export const NotFoundPage = () => {
     const navigate = useNavigate();
     return (
         <>
-            <div className="flex justify-center items-center h-screen flex-col">
+            <div className="flex justify-center items-center h-screen flex-col container">
                 <h1 className="font-black text-9xl text-roboto m-2 mt-3 animate-bounce">404</h1>
                 <h3 className="text-3xl font-medium">We searched the entire site</h3>
-                <p className="m-2 mb-5 text-center">It looks like this page doesn't exist. If you still need help,
-                    contact<br/> technical support.</p>
+                <div className="w-full md:w-12/12">
+                    <p className="m-2 mb-5 text-center">It looks like this page doesn't exist. If you still need help,
+                        contact technical support.</p>
+                </div>
                 <Button className="w-96" onClick={() => navigate('/')}>Return to the main page</Button>
             </div>
         </>
