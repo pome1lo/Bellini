c140cbe463a17e618a648d3453e8ddd372d805f7
pome1lo <paaworker@gmail.com>
2024-08-19 12:42:38 +0300

    update register service interface / create code verification dto

--- Bellini/BusinessLogicLayer/Services/Interfaces/IRegisterService.cs
+++ Bellini/BusinessLogicLayer/Services/Interfaces/IRegisterService.cs
@@ -1,9 +1,13 @@
 ﻿using BusinessLogic.Services.DTOs;
+using BusinessLogicLayer.Services.DTOs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BusinessLogic.Services.Interfaces
 {
     public interface IRegisterService
     {
+        Task<IActionResult> CheckEmailAsync(string email, CancellationToken cancellationToken = default);
+        Task<IActionResult> VerifyCodeAsync(CodeVerificationDto codeVerificationDto, CancellationToken cancellationToken = default);
         Task<UserDto> RegisterUserAsync(RegisterDto registerDto, CancellationToken cancellationToken = default);
     }
 }
