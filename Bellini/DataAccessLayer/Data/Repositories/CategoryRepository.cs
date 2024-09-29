36ded622e9295fd4ffc1f39277ce72ed5d483175
Alexey <paaworker@gmail.com>
2024-09-29 11:56:39 +0300

    create category repository

---
+++ Bellini/DataAccessLayer/Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using DataAccess.Data;
+using DataAccess.Data.Interfaces;
+using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Data.Repositories
+{
+    public class CategoryRepository : IRepository<Category>
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryRepository(AppDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public async Task<IEnumerable<Category>> GetElementsAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.Categories.AsNoTracking().ToListAsync(cancellationToken);
+        }
+
+        public async Task<Category> GetItemAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _context.Categories.FindAsync(new object[] { id }, cancellationToken);
+        }
+
+        public async Task CreateAsync(Category item, CancellationToken cancellationToken = default)
+        {
+            await _context.Categories.AddAsync(item, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task UpdateAsync(int id, Category item, CancellationToken cancellationToken = default)
+        {
+            await _context.Categories.Where(e => e.Id == id)
+                .ExecuteUpdateAsync(s => s
+                    .SetProperty(e => e.Name, item.Name),
+                    cancellationToken
+                );
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task DeleteAsync(int id, CancellationToken cancellationToken = default)
+        {
+            await _context.Categories.Where(e => e.Id == id).ExecuteDeleteAsync(cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
