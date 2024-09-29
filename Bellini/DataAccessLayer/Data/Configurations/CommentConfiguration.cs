0831d018f3986613152646f2c606e532c77388a2
Alexey <paaworker@gmail.com>
2024-09-29 11:41:07 +0300

    create Comment and Player configuration

---
+++ Bellini/DataAccessLayer/Data/Configurations/CommentConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccessLayer.Data.Configurations
+{
+    public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.ToTable("Comments");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Content)
+                   .IsRequired()
+                   .HasMaxLength(500);
+
+            builder.HasOne(c => c.Game)
+                   .WithMany(g => g.Comments)
+                   .HasForeignKey(c => c.GameId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ Bellini/DataAccessLayer/Data/Configurations/PlayerConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccessLayer.Data.Configurations
+{
+    public class PlayerConfiguration : IEntityTypeConfiguration<Player>
+    {
+        public void Configure(EntityTypeBuilder<Player> builder)
+        {
+            builder.ToTable("Players");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.HasOne(p => p.Game)
+                   .WithMany(g => g.Players)
+                   .HasForeignKey(p => p.GameId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
