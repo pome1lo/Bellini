2beb528213c5662fa33c26be680ec01bd99a9b6f
Alexey <paaworker@gmail.com>
2024-10-07 20:50:03 +0300

    update ApiGateway routes / fix validator

--- Bellini/BusinessLogicLayer/Services/Validators/UpdateProfileDtoValidator.cs
+++ Bellini/BusinessLogicLayer/Services/Validators/UpdateProfileDtoValidator.cs
@@ -6,7 +6,7 @@ namespace BusinessLogicLayer.Services.Validators
     public class UpdateProfileDtoValidator : AbstractValidator<UpdateProfileDto>
     {
         public UpdateProfileDtoValidator()
-        {  
+        {
             RuleFor(x => x.FirstName)
                 .Length(1, 50).WithMessage("First name must be between 1 and 50 characters.")
                 .When(x => !string.IsNullOrEmpty(x.FirstName));
@@ -22,6 +22,13 @@ namespace BusinessLogicLayer.Services.Validators
             RuleFor(x => x.ProfileImageUrl)
                 .Must(uri => Uri.IsWellFormedUriString(uri, UriKind.Absolute)).WithMessage("Profile image URL is not valid.")
                 .When(x => !string.IsNullOrEmpty(x.ProfileImageUrl));
+
+            RuleFor(x => x)
+                .Must(x => !string.IsNullOrEmpty(x.FirstName)
+                        || !string.IsNullOrEmpty(x.LastName)
+                        || x.DateOfBirth.HasValue
+                        || !string.IsNullOrEmpty(x.ProfileImageUrl))
+                .WithMessage("At least one field must be provided for the update.");
         }
     }
 }
