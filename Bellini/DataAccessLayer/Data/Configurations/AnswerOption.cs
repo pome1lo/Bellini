298f79a4d9fa418f09f9f968fcd05ff7c5e6ab73
Alexey <paaworker@gmail.com>
2024-10-13 19:21:36 +0300

    add AnswerOption

---
+++ Bellini/DataAccessLayer/Data/Configurations/AnswerOption.cs
@@ -0,0 +1,28 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccessLayer.Data.Configurations
+{
+    public class AnswerOptionConfiguration : IEntityTypeConfiguration<AnswerOption>
+    {
+        public void Configure(EntityTypeBuilder<AnswerOption> builder)
+        {
+            builder.ToTable("AnswerOptions");
+
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.Text)
+                   .IsRequired()
+                   .HasMaxLength(200);
+
+            builder.Property(a => a.IsCorrect)
+                   .IsRequired();
+
+            builder.HasOne(a => a.Question)
+                   .WithMany(q => q.AnswerOptions)
+                   .HasForeignKey(a => a.QuestionId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
