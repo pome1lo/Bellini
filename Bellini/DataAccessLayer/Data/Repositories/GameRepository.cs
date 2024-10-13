43ccd1d3da38c6ef7e6917c468ff751eceef61dc
Alexey <paaworker@gmail.com>
2024-10-13 19:34:56 +0300

    update GameRepository

--- Bellini/DataAccessLayer/Data/Repositories/GameRepository.cs
+++ Bellini/DataAccessLayer/Data/Repositories/GameRepository.cs
@@ -14,56 +14,53 @@ namespace DataAccessLayer.Data.Repositories
             _context = dbContext;
         }
 
-        // Получение всех игр
         public async Task<IEnumerable<Game>> GetElementsAsync(CancellationToken cancellationToken = default)
         {
             return await _context.Games
                                  .AsNoTracking()
-                                 .Include(g => g.Status) // Включаем статус игры
-                                 .Include(g => g.Players) // Включаем игроков
-                                 .Include(g => g.Comments) // Включаем комментарии
+                                 .Include(g => g.Status)
+                                 .Include(g => g.Players)
+                                 .Include(g => g.Comments)
+                                 .Include(g => g.Questions)
                                  .ToListAsync(cancellationToken);
         }
 
-        // Получение конкретной игры по Id
         public async Task<Game> GetItemAsync(int id, CancellationToken cancellationToken = default)
         {
             return await _context.Games
-                                 .Include(g => g.Status) // Включаем статус игры
-                                 .Include(g => g.Players) // Включаем игроков
-                                 .Include(g => g.Comments) // Включаем комментарии
+                                 .Include(g => g.Status)
+                                 .Include(g => g.Players)
+                                 .Include(g => g.Comments)
+                                 .Include(g => g.Questions)
                                  .FirstOrDefaultAsync(g => g.Id == id, cancellationToken);
         }
 
-        // Создание новой игры
         public async Task CreateAsync(Game item, CancellationToken cancellationToken = default)
         {
             await _context.Games.AddAsync(item, cancellationToken);
             await _context.SaveChangesAsync(cancellationToken);
         }
 
-        // Обновление существующей игры
         public async Task UpdateAsync(int id, Game item, CancellationToken cancellationToken = default)
         {
-            // Найдем игру по id
             var gameToUpdate = await _context.Games.FindAsync(id);
             if (gameToUpdate != null)
             {
-                // Обновляем поля игры
                 gameToUpdate.GameName = item.GameName;
                 gameToUpdate.HostId = item.HostId;
                 gameToUpdate.StartTime = item.StartTime;
                 gameToUpdate.MaxPlayers = item.MaxPlayers;
-                gameToUpdate.IsPrivate = item.IsPrivate; // Учитываем приватность игры
-                gameToUpdate.RoomPassword = item.RoomPassword; // Учитываем пароль комнаты
+                gameToUpdate.IsPrivate = item.IsPrivate;
+                gameToUpdate.RoomPassword = item.RoomPassword;
                 gameToUpdate.GameCoverImageUrl = item.GameCoverImageUrl;
-                gameToUpdate.Status = item.Status; // Обновляем статус игры
+                gameToUpdate.Status = item.Status;
+
+                gameToUpdate.Questions = item.Questions;
 
                 await _context.SaveChangesAsync(cancellationToken);
             }
         }
 
-        // Удаление игры
         public async Task DeleteAsync(int id, CancellationToken cancellationToken = default)
         {
             var gameToDelete = await _context.Games.FindAsync(id);
