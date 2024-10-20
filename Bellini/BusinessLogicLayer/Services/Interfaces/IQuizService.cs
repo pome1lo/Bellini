f2d5d0606e6bfe3129afd060328a50b848856693
Alexey <paaworker@gmail.com>
2024-10-20 13:19:11 +0300

    created the IQuizService interface

---
+++ Bellini/BusinessLogicLayer/Services/Interfaces/IQuizService.cs
@@ -0,0 +1,10 @@
+﻿using DataAccessLayer.Models;
+
+namespace BusinessLogicLayer.Services.Interfaces
+{
+    public interface IQuizService
+    {
+        Task<IEnumerable<Quiz>> GetAllQuizzesAsync(CancellationToken cancellationToken = default);
+        Task<Quiz> GetQuizByIdAsync(int id, CancellationToken cancellationToken = default);
+    }
+}
