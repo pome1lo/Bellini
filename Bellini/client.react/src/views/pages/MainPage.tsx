f03598db013cb7f5028cf1a27c51b874451a7d26
Alexey <paaworker@gmail.com>
2024-11-05 14:33:02 +0300

    update footer

--- Bellini/client.react/src/views/pages/MainPage.tsx
+++ Bellini/client.react/src/views/pages/MainPage.tsx
@@ -14,17 +14,17 @@ export const MainPage = () => {
     return (
         <>
             <div className="flex flex-col items-center p-7">
-                <div className="text-center max-w-[600px] mt-32">
-                    <h1 className="text-6xl font-bold">The designer<br/>of marketing quizzes</h1>
-                    <p className="mt-3 mb-4 text-xl">Quizzes create a Business-Client dialogue and help visitors and
+                <div className="text-center max-w-[600px] sm:mt-32">
+                    <h1 className="text-3xl sm:text-6xl font-bold">The designer<br/>of marketing quizzes</h1>
+                    <p className="mt-3 mb-4 sm:text-xl">Quizzes create a Business-Client dialogue and help visitors and
                         companies better understand each other. Hence the sales growth. More than 200 million
                         applications have been received by our clients</p>
                     <Button className="min-w-32">Try it - it's free</Button>
                 </div>
                 <img src={bg} className="w-4/5" alt=""/>
                 <div className="text-start items-start w-full max-w-[1090px] mt-10 mb-10">
-                    <h1 className="text-5xl font-bold">Not only a tool, but also a method</h1>
-                    <h3 className="text-2xl">Let's tell you how to make a quiz the most effective tool in marketing</h3>
+                    <h1 className="text-2xl sm:text-5xl font-bold">Not only a tool, but also a method</h1>
+                    <h3 className="sm:text-2xl">Let's tell you how to make a quiz the most effective tool in marketing</h3>
                 </div>
                 <div className="flex gap-5 flex-wrap justify-evenly max-w-[1100px]">
                     <Card className="w-[350px]" data-aos="flip-left" data-aos-duration="1000">
--- Bellini/client.react/src/views/partials/Footer.tsx
+++ Bellini/client.react/src/views/partials/Footer.tsx
@@ -1,4 +1,5 @@
 import {Button} from "@/components/ui/button.tsx";
+import {Instagram, Linkedin, Twitter, Youtube} from "lucide-react";
 
 export const Footer = () => {
     return (
@@ -6,15 +7,41 @@ export const Footer = () => {
             <footer className="bg-black text-white py-8">
                 <div className="mx-auto px-4">
                     <div className="flex flex-col md:flex-row justify-between">
-                        <div className=" order-2 md:mb-0 md:order-1">
-                            <h2 className="text-3xl text-shrikhand">Bellini</h2>
-                            <p className="text-gray-400 text-sm">© 2024 Bellini. All rights reserved.</p>
+                        <div className="flex justify-between order-2 md:mb-0 md:order-1">
+                            <div>
+                                <h2 className="text-3xl text-shrikhand">Bellini</h2>
+                                <p className="text-gray-400 text-sm">© 2024 Bellini. All rights reserved.</p>
+                            </div>
+                            <div className="md:hidden block">
+                                <Button variant="link">
+                                    <Instagram className="w-5 h-5 hover:scale-125 transition duration-500"/>
+                                </Button>
+                                <Button variant="link">
+                                    <Twitter className="w-5 h-5 hover:scale-125 transition duration-500"/>
+                                </Button> 
+                                <Button variant="link">
+                                    <Linkedin className="w-5 h-5 hover:scale-125 transition duration-500"/>
+                                </Button>
+                            </div>
                         </div>
-                        <div
-                            className="flex md:order-2 order-1 sm:space-x-4 flex-col sm:flex-row">
-                            <Button variant="link" className="text-start text-white">Contacts</Button>
-                            <Button variant="link" className="text-white">About</Button>
-                            <Button variant="link" className="text-white">Support</Button>
+                        <div className="flex justify-end sm:flex-wrap md:order-2 order-1 sm:space-x-4 flex-col sm:flex-row">
+                            <div className="md:block hidden w-full text-right">
+                                <Button variant="link">
+                                    <Instagram className="hover:scale-125 transition duration-500"/>
+                                </Button>
+                                <Button variant="link">
+                                    <Twitter className="hover:scale-125 transition duration-500"/>
+                                </Button>
+                                <Button variant="link">
+                                    <Linkedin className="hover:scale-125 transition duration-500"/>
+                                </Button>
+                            </div>
+                            <div>
+                                <Button variant="link" className="text-start text-white">Contacts</Button>
+                                <Button variant="link" className="text-white">About</Button>
+                                <Button variant="link" className="text-white">Support</Button>
+                                <Button variant="link" className="text-white">Contacts</Button>
+                            </div>
                         </div>
                     </div>
                 </div>
