7b8daea1b4dc5d964ca1920fe55e46b6ef011181
Alexey <paaworker@gmail.com>
2024-12-23 21:57:25 +0300

    add https to react

--- Bellini/client.react/vite.config.ts
+++ Bellini/client.react/vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+      https: {
+          key: './certs/local/LAB.key',
+          cert: './certs/local/LAB.crt',
+      },
+  },
   plugins: [react()],
   build: {
     outDir: 'dist',
