0ab3949ba1299634f35ef6e36a2fa428f36efdb6
pome1lo <paaworker@gmail.com>
2025-03-13 14:43:12 +0300

    Added new models

---
+++ Bellini/DataAccessLayer/Models/UserAchievement.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+using UtilsModelsLibrary.Enums;
+
+namespace DataAccessLayer.Models
+{
+    public class UserAchievement : BaseModel
+    {
+        public int UserId { get; set; }
+        [JsonIgnore] public User User { get; set; } = null!;
+        public AchievementType Achievement { get; set; }
+        public DateTime AchievedAt { get; set; }
+    }
+}
---
+++ Bellini/DataAccessLayer/Models/UserStatistics.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DataAccessLayer.Models
+{
+    public class UserStatistics : BaseModel
+    {
+        public int UserId { get; set; }
+        [JsonIgnore] public User User { get; set; } = null!;
+        public int ProfileEdits { get; set; } = default;
+        public int AvatarsSet { get; set; } = default;
+        public int QuizzesCompleted { get; set; } = default;
+        public int GamesPlayed { get; set; } = default;
+        public int QuizComments { get; set; } = default;
+        public int GameComments { get; set; } = default;
+        public int QuizzesCreated { get; set; } = default;
+        public int QuestionsCreated { get; set; } = default;
+    }
+}
