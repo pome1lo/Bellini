229da9a99ab069f4b3d27eb2854211b988d2ea35
pome1lo <paaworker@gmail.com>
2025-03-13 22:44:27 +0300

    Added a cluster of extensions and updated attributes

---
+++ Bellini/UtilsModelsLibrary/Attributes/RolesOnlyAuthorizeAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+using UtilsModelsLibrary.Base;
+using UtilsModelsLibrary.Enums;
+
+namespace UtilsModelsLibrary.Attributes
+{
+    public class RolesOnlyAuthorizeAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public Roles RolePropertyName { get; set; }
+
+        public RolesOnlyAuthorizeAttribute(Roles RolePropertyName)
+        {
+            this.RolePropertyName = RolePropertyName;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var roleClaim = context.HttpContext.User.FindFirst(ClaimTypes.Role);
+            if (roleClaim is null || (!roleClaim.Value.Equals(RolePropertyName.GetDescription()) && !roleClaim.Value.Equals(Enums.Roles.Admin.GetDescription())))
+            {
+                context.Result = new ForbidResult();
+            }
+        }
+    }
+}
---
+++ Bellini/UtilsModelsLibrary/Extensions/TokenHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace UtilsModelsLibrary.Extensions
+{
+    public static class TokenHelper
+    {
+        public static string GetParameterFromToken(HttpContext context, string claim = ClaimTypes.NameIdentifier)
+        {
+            var paramClaim = context.User.FindFirst(claim);
+            if (paramClaim is null)
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            return paramClaim.Value;
+        }
+    }
+}
